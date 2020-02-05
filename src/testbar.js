df25a34eab5794e644d7f1957a84f397149a53e2
Sherry Prawiro <sherrp@cs.washington.edu>
2020-02-04 17:08:37 -0800

    initial bar chart working

--- src/testbar.js
+++ src/testbar.js
@@ -1,60 +1,56 @@
 'use strict';
 
-var rec = d3.select(".myRec")
-  .append('rect')
-  .attr('width', 20)
-  .attr('height', 20)
-  .attr('fill', 'blue')
-
 const margin = {top: 20, right: 30, bottom: 40, left: 260};
-const width = 960 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+const width = 650 - margin.left - margin.right;
+const height = 350 - margin.top - margin.bottom;
 const percentFormat = d3.format('.0%');
 const leftPadding = 5;
+var colorScale = d3.scaleOrdinal(d3["schemeCategory20"]);
+
 
 const delay = function(d, i) {
-return i * 40;
+    return i * 40;
 };
 
 function sortData(data) {
-return data.sort((a, b) => b.value - a.value);
+    return data.sort((a, b) => b.value - a.value);
 }
 
 function removeGeoAreasWithNoData(data) {
-return data.filter(d => d.value);
+    return data.filter(d => d.value);
 }
 
 function prepareData(data) {
-return data.reduce((accumulator, d) => {
-    Object.keys(d).forEach((k) => {
-    if (!Number.isInteger(+k)) { return; }
-    let value;
-    if (d[+k] === '..') {
-        value = 0;
-    } else {
-        value = +d[+k] / 100;
-    }
-    const newEntry = {
-        value,
-        geoCode: d.CountryCode,
-        geoName: d.Country,
-    };
-    if (accumulator[+k]) {
-        accumulator[+k].push(newEntry);
-    } else {
-        accumulator[+k] = [newEntry];
-    }
-    });
-    return accumulator;
-}, {});
+    return data.reduce((accumulator, d) => {
+        Object.keys(d).forEach((k) => {
+        if (!Number.isInteger(+k)) { return; }
+        let value;
+        if (d[+k] === '..') {
+            value = 0;
+        } else {
+            value = +d[+k] / 100;
+        }
+        const newEntry = {
+            value,
+            geoCode: d.CountryCode,
+            geoName: d.Country,
+        };
+        if (accumulator[+k]) {
+            accumulator[+k].push(newEntry);
+        } else {
+            accumulator[+k] = [newEntry];
+        }
+        });
+        return accumulator;
+    }, {});
 }
 
 function xAccessor(d) {
-return d.value;
+    return d.value;
 }
 
 function yAccessor(d) {
-return d.geoName;
+    return d.geoName;
 }
 
 const xScale = d3.scaleLinear()
@@ -66,17 +62,17 @@ const yScale = d3.scaleBand()
     .padding(0.1);
 
 function drawXAxis(el) {
-el.append('g')
-    .attr('class', 'axis axis--x')
-    .attr('transform', `translate(${leftPadding},${height})`)
-    .call(d3.axisBottom(xScale).tickFormat(percentFormat));
+    el.append('g')
+        .attr('class', 'axis axis--x')
+        .attr('transform', `translate(${leftPadding},${height})`)
+        .call(d3.axisBottom(xScale).tickFormat(percentFormat));
 }
 
 function drawYAxis(el, data, t) {
-let axis = el.select('.axis--y');
-if (axis.empty()) {
-    axis = el.append('g')
-    .attr('class', 'axis axis--y');
+    let axis = el.select('.axis--y');
+    if (axis.empty()) {
+        axis = el.append('g')
+        .attr('class', 'axis axis--y');
 }
 
 axis.transition(t)
@@ -94,7 +90,8 @@ if (barsG.empty()) {
 
 const bars = barsG
     .selectAll('.bar')
-    .data(data, yAccessor);
+    .data(data, yAccessor)
+
 bars.exit()
     .remove();
 bars.enter()
@@ -105,6 +102,8 @@ bars.enter()
     .attr('y', d => yScale(yAccessor(d)))
     .attr('width', d => xScale(xAccessor(d)))
     .attr('height', yScale.bandwidth())
+    .style('fill', function(d, i) {
+        return colorScale(d.geoCode); })
     .delay(delay);
 }
 
@@ -124,27 +123,32 @@ fetch('https://gist.githubusercontent.com/deciob/ffd5c65629e43449246cb80a0af280c
     let selectedData = removeGeoAreasWithNoData(sortData(data[startYear]));
     let geoAreas = selectedData.map(yAccessor);
 
-    d3.select('.year').text(startYear);
-
     yScale.domain(geoAreas);
     drawXAxis(svg, selectedData);
     drawYAxis(svg, selectedData);
     drawBars(svg, selectedData);
 
-    const interval = d3.interval(() => {
-    const t = d3.transition().duration(400);
+    d3.select("#year").selectAll("option")
+		.data(years)
+	    .enter().append("option")
+        .attr("value", function(d){
+            return d;
+        })
+        .text(function(d){
+            return d;
+        });
+    
+    d3.select("#year").on("change", function(){
+        const t = d3.transition().duration(400);
 
-    startYear += 1;
-    selectedData = removeGeoAreasWithNoData(sortData(data[startYear]));
+        var year = $(this).val();
 
-    d3.select('.year').text(startYear);
+        selectedData = removeGeoAreasWithNoData(sortData(data[year]));
 
-    yScale.domain(selectedData.map(yAccessor));
-    drawYAxis(svg, selectedData, t);
-    drawBars(svg, selectedData, t);
+        d3.select('.year').text(year);
 
-    if (startYear === lastYear) {
-        interval.stop();
-    }
-    }, 1000);
+        yScale.domain(selectedData.map(yAccessor));
+        drawYAxis(svg, selectedData, t);
+        drawBars(svg, selectedData, t);
+    });
 });
