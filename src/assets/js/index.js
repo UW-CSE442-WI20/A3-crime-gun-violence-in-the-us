d5caf7a2dd06ac3238af7a0a1a1188a714bf2893
Chenghao Zhu <chengz27@cs.washington.edu>
2020-02-05 22:10:10 -0800

    repush year slider

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -704,6 +704,107 @@ function drawMap(selectYear) {
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // LAPD division : http://geohub.lacity.org/datasets/lapd-divisions/data?geometry=-119.399%2C33.821%2C-117.421%2C34.220&orderBy=PREC
+
+////////// slider //////////
+var formatDateIntoYear = d3.timeFormat("%Y");
+var formatDate = d3.timeFormat("%Y");
+var parseDate = d3.timeParse("%y");
+
+var startDate = new Date("2010-01-01"),
+    endDate = new Date("2017-12-01");
+
+var margin = {top: 20, right: 30, bottom: 10, left: 50},
+    width = 650 - margin.left - margin.right,
+    height = 250 - margin.top - margin.bottom;
+
+var svg = d3.select("#vis")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);  
+
+// slider
+var moving = false;
+var currentValue = 0;
+var targetValue = width;
+
+var playButton = d3.select("#play-button");
+    
+var x = d3.scaleTime()
+    .domain([startDate, endDate])
+    .range([0, targetValue])
+    .clamp(true);
+
+var slider = svg.append("g")
+    .attr("class", "slider")
+    .attr("transform", "translate(" + margin.left + "," + height/5 + ")");
+
+slider.append("line")
+    .attr("class", "track")
+    .attr("x1", x.range()[0])
+    .attr("x2", x.range()[1])
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-inset")
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-overlay")
+    .call(d3.drag()
+        .on("start.interrupt", function() { slider.interrupt(); })
+        .on("start drag", function() {
+          currentValue = d3.event.x;
+          update(x.invert(currentValue)); 
+        })
+    );
+
+slider.insert("g", ".track-overlay")
+    .attr("class", "ticks")
+    .attr("transform", "translate(0," + 18 + ")")
+  .selectAll("text")
+    .data(x.ticks(10))
+    .enter()
+    .append("text")
+    .attr("x", x)
+    .attr("y", 10)
+    .attr("text-anchor", "middle")
+    .text(function(d) { return formatDateIntoYear(d); });
+
+var handle = slider.insert("circle", ".track-overlay")
+    .attr("class", "handle")
+    .attr("r", 9);
+
+var label = slider.append("text")  
+    .attr("class", "label")
+    .attr("text-anchor", "middle")
+    .text(formatDate(startDate))
+    .attr("transform", "translate(0," + (-25) + ")")
+
+function step() {
+    update(x.invert(currentValue));
+    currentValue = currentValue + (targetValue/151);
+    if (currentValue > targetValue) {
+      moving = false;
+      currentValue = 0;
+      clearInterval(timer);
+      // timer = 0;
+      playButton.text("Play");
+      console.log("Slider moving: " + moving);
+    }
+}
+
+function update(h) {
+    // update position and text of label according to slider scale
+    handle.attr("cx", x(h));
+    label
+      .attr("x", x(h))
+      .text(formatDate(h));
+  
+    // filter data set and redraw plot
+    var newData = dataset.filter(function(d) {
+      return d.date < h;
+    })
+    drawPlot(newData);
+}
+////////// slider ends //////////
+
+
 // bar-chart
 
 var margin = {top: 20, right: 30, bottom: 40, left: 260};
